0d17fc90670cd8986c8fb87e577337c95d7892c6
Najib Qunoo <150752398+Mathe-Genie1@users.noreply.github.com>
2024-03-31 19:23:17 +0200

    Update ostern.py

--- ostern.py
+++ ostern.py
@@ -1,18 +1,14 @@
 def calculate_easter(year):
     a = year % 19
-    b = year // 100
-    c = year % 100
-    d = b // 4
-    e = b % 4
-    f = (b + 8) // 25
-    g = (b - f + 1) // 3
-    h = (19 * a + b - d - g + 15) % 30
-    i = c // 4
-    k = c % 4
-    l = (32 + 2 * e + 2 * i - h - k) % 7
-    m = (a + 11 * h + 22 * l) // 451
-    month = (h + l - 7 * m + 114) // 31
-    day = ((h + l - 7 * m + 114) % 31) + 1
+    b = year % 4
+    c = year % 7
+    d = ((19 * a) + 24) % 30
+    e = ((2 * b) +  (4 * c) + (6 * d) + 5) % 7
+    f = 22 + d + e
+    day = f % 32
+    if f > 31 :
+        month = 4
+    else :
+        month = 3
     return day, month
-
 print(calculate_easter(2024))
